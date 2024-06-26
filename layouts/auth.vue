fb498aa017dcef7e76a9ba72e5d089d8aa4f8397
Kissuki <andhika.smandading@gmail.com>
2024-06-26 09:00:51 +0700

    feat: init auth Layout

---
+++ layouts/auth.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nav>
+      This is a nav
+    </nav>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,7 @@
 </template>
 
 <script lang="ts" setup>
-
+definePageMeta({
+  layout: 'auth'
+})
 </script>
